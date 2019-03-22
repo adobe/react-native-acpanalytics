e8a94d0fa4f73dd81603aec0aee14fca8c7e708e
nporter <nporter@adobe.com>
2019-03-22 12:10:32 -0600

    Checkin sample

---
+++ sample/ACPAnalyticsSample/README.md
@@ -0,0 +1,21 @@
+# ACPCoreSample
+
+How to run the sample app:
+
+Begin by installing the npm dependencies for the sample app (ensure you have ran `npm install` in the parent directory for the actual module)
+`npm install`
+
+To run the app:
+
+`react-native run-ios (For iOS) or you can open ios/ACPCoreSample.xcodeproj and hit run`
+
+`react-native run-android (For Android, be sure to have an emulator running: https://developer.android.com/studio/run/emulator-commandline)`
+
+### Troubleshooting
+
+If you're having issues running the sample app, ensure you can at least run a default react native app.
+
+```
+react-native init MyReactApp
+react-native run-ios or react-native run-android
+```
---
+++ sample/ACPAnalyticsSample/android/app/src/main/java/com/acpcoresample/MainApplication.java
@@ -0,0 +1,51 @@
+package com.acpcoresample;
+
+import android.app.Application;
+
+import com.facebook.react.ReactApplication;
+import com.adobe.marketing.mobile.reactnative.analytics.RCTACPAnalyticsPackage;
+import com.adobe.marketing.mobile.reactnative.RCTACPCorePackage;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
+import com.adobe.marketing.mobile.reactnative.RCTACPCoreModule;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainApplication extends Application implements ReactApplication {
+
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    @Override
+    public boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
+    }
+
+    @Override
+    protected List<ReactPackage> getPackages() {
+      return Arrays.<ReactPackage>asList(
+          new MainReactPackage(),
+            new RCTACPAnalyticsPackage(),
+            new RCTACPCorePackage()
+      );
+    }
+
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
+    }
+  };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
+    RCTACPCoreModule.setApplication(this);
+  }
+}
