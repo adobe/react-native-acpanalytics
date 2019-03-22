e4ac3e453222f10a6349f57727ec15ad33bbe6a1
nporter <nporter@adobe.com>
2019-03-22 14:19:33 -0600

    Add copyright headers and try/catch on register extension

--- android/src/main/java/com/adobe/marketing/mobile/reactnative/analytics/RCTACPAnalyticsModule.java
+++ android/src/main/java/com/adobe/marketing/mobile/reactnative/analytics/RCTACPAnalyticsModule.java
@@ -1,8 +1,26 @@
-
+/*************************************************************************
+ * ADOBE CONFIDENTIAL
+ * ___________________
+ *
+ * Copyright 2019 Adobe
+ * All Rights Reserved.
+ *
+ * NOTICE: All information contained herein is, and remains
+ * the property of Adobe and its suppliers, if any. The intellectual
+ * and technical concepts contained herein are proprietary to Adobe
+ * and its suppliers and are protected by all applicable intellectual
+ * property laws, including trade secret and copyright laws.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Adobe.
+ **************************************************************************/
 package com.adobe.marketing.mobile.reactnative.analytics;
+
+import android.util.Log;
 
 import com.adobe.marketing.mobile.AdobeCallback;
 import com.adobe.marketing.mobile.Analytics;
+import com.adobe.marketing.mobile.InvalidInitException;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
@@ -29,8 +47,12 @@ public class RCTACPAnalyticsModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void registerExtension() throws Exception {
-      Analytics.registerExtension();
+    public void registerExtension() {
+        try {
+            Analytics.registerExtension();
+        } catch (InvalidInitException e) {
+            Log.d(getName(), "Registering Analytics extension failed with error: " + e.getMessage());
+        }
     }
 
     @ReactMethod
