f507c52a250191161a1100611310bfbfec7ca021
Nick Porter <nporter@adobe.com>
2019-05-23 09:13:27 -0600

    Update copy script

--- sample/ACPAnalyticsSample/copy-changes-to-sample.sh
+++ sample/ACPAnalyticsSample/copy-changes-to-sample.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 set -e
 
+EXTENSION_NAME=react-native-acpanalytics
+
 echo 'Installing npm packages'
 npm install
 
 echo 'Unlinking React Native Module'
-react-native unlink @adobe/react-native-acpanalytics
+react-native unlink @adobe/$EXTENSION_NAME
 
 echo 'Copying React Native Module into Sample App'
-rm -r node_modules/@adobe/react-native-acpanalytics
-mkdir node_modules/@adobe/react-native-acpanalytics
+rm -r node_modules/@adobe/$EXTENSION_NAME
+mkdir node_modules/@adobe/$EXTENSION_NAME
 cd ../.. && npm pack
 cd sample/ACP*Sample/
 cp ../../*.tgz node_modules/@adobe/
 tar -xvzf node_modules/@adobe/*.tgz --directory=node_modules/@adobe/
 rm node_modules/@adobe/*.tgz
-rm -r node_modules/@adobe/react-native-acpanalytics
-mv node_modules/@adobe/package node_modules/@adobe/react-native-acpanalytics
+rm -r node_modules/@adobe/$EXTENSION_NAME
+mv node_modules/@adobe/package node_modules/@adobe/$EXTENSION_NAME
 
 echo 'Linking'
 react-native link
