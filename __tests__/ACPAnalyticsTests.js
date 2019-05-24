90dbb21ec33d22c75dc21b2ef2ed5014fca587fc
Nick Porter <43650450+nporter-adbe@users.noreply.github.com>
2019-05-24 10:22:14 -0600

    First attempt at CI (#2)
    * First attempt at CI
    
    * Trigger CI
    
    * Add working directory
    
    * Update working directorys
    
    * Fix working directory
    
    * New working directory
    
    * Use project name in working dir
    
    * Change circle CI verison
    
    * Update circleci config
    
    * Update circleci config
    
    * Update circleci config
    
    * Update circleci config
    
    * Update circleci config
    
    * Update circleci config
    
    * Update circleci config
    
    * Add gemfile
    
    * Build ios proejct
    
    * Cache cocoapods
    
    * Fix pod path in cache
    
    * Add make target to update iOS lib
    
    * Remove unused code from circleCI config
    
    * Add podfile.lock
    
    * Add script to update android version number
    
    * Setup sample app build
    
    * Build android sample app
    
    * Diff job to prep repo
    
    * Unit tests (#4)
    
    * Tests running
    
    * Use babel flow preset
    
    * rename job
    
    * Rename test file
    
    * Comment out test job
    
    * Add test job
    
    * Add machine type
    
    * Add docker image
    
    * Setup sample app build
    
    * fix circleCI config
    
    * Use macOS for unit tests
    
    * Add xcode version
    
    * Add ls command
    
    * update makefile
    
    * Just use jest
    
    * don't use makefile
    
    * Use dev node
    
    * install jest
    
    * add jest cli as dev dependency
    
    * Install jest cli on workflow
    
    * Run jest from node modules
    
    * Run jest from node modules
    
    * Update config for circleci
    
    * Update makefile
    
    * Fix jest version
    
    * Get jest version
    
    * Fix jest version
    
    * Update readme badges
    
    * Update README.md
    
    * Update copy script
    
    * Update README.md
    
    * Add Adobe copyright headers
    
    * Add build sample app in CI steps (#5)
    
    * Add step to build sample app
    
    * install RN cli
    
    * Change gradle command
    
    * Define android SDK location
    
    * Accept Android SDK licenses
    
    * Accept SDK
    
    * Add docker image
    
    * Try to accept android terms
    
    * Use yes
    
    * Try seperate jobs
    
    * Fix circle config
    
    * Fix code signing
    
    * Add android sample app job
    
    * Dont install react cli globally
    
    * Add sudo
    
    * Update prepublish script
    
    * Run full CI sequence
    
    * Fix up sample app xcode project
    
    * Speed up iOS build
    
    * Try to improve build time
    
    * Clone cocopapods into right path
    
    * Add mkdir command
    
    * Run all jobs

---
+++ __tests__/ACPAnalyticsTests.js
@@ -0,0 +1,69 @@
+/*
+Copyright 2019 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+
+@format
+*/
+
+import { NativeModules } from 'react-native';
+import ACPAnalytics from '../js/ACPAnalytics';
+
+describe('ACPAnalytics', () => {
+
+  test('extensionVersion is called', async () => {
+    const spy = jest.spyOn(NativeModules.ACPAnalytics, 'extensionVersion');
+    await ACPAnalytics.extensionVersion();
+    expect(spy).toHaveBeenCalled();
+  });
+
+  test('registerExtension is called', async () => {
+    const spy = jest.spyOn(NativeModules.ACPAnalytics, 'registerExtension');
+    await ACPAnalytics.registerExtension();
+    expect(spy).toHaveBeenCalled();
+  });
+
+  test('getTrackingIdentifier is called', async () => {
+    const spy = jest.spyOn(NativeModules.ACPAnalytics, 'getTrackingIdentifier');
+    await ACPAnalytics.getTrackingIdentifier();
+    expect(spy).toHaveBeenCalled();
+  });
+
+  test('getQueueSize is called', async () => {
+    const spy = jest.spyOn(NativeModules.ACPAnalytics, 'getQueueSize');
+    await ACPAnalytics.getQueueSize();
+    expect(spy).toHaveBeenCalled();
+  });
+
+  test('clearQueue is called', async () => {
+    const spy = jest.spyOn(NativeModules.ACPAnalytics, 'clearQueue');
+    await ACPAnalytics.clearQueue();
+    expect(spy).toHaveBeenCalled();
+  });
+
+  test('sendQueuedHits is called', async () => {
+    const spy = jest.spyOn(NativeModules.ACPAnalytics, 'sendQueuedHits');
+    await ACPAnalytics.sendQueuedHits();
+    expect(spy).toHaveBeenCalled();
+  });
+
+  test('getVisitorIdentifier is called', async () => {
+    const spy = jest.spyOn(NativeModules.ACPAnalytics, 'getVisitorIdentifier');
+    await ACPAnalytics.getVisitorIdentifier();
+    expect(spy).toHaveBeenCalled();
+  });
+
+  test('setVisitorIdentifier is called with correct parameters', async () => {
+    const spy = jest.spyOn(NativeModules.ACPAnalytics, 'setVisitorIdentifier');
+    let vid = 'vid'
+    await ACPAnalytics.setVisitorIdentifier(vid);
+    expect(spy).toHaveBeenCalledWith(vid);
+  });
+
+});
